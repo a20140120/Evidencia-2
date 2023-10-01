c32cd204fb68ccc62bf669651f784e40c92933a3
a20140120 <131292918+a20140120@users.noreply.github.com>
2023-10-01 06:52:24 -0500

    Add files via upload

---
+++ condicionales.js
@@ -0,0 +1,24 @@
+/*
+alert("ni hao")
+num1=prompt("Ingrsa un número")
+
+if (num1>0) {
+    console.log("El número",num1,"es positivo")
+}
+else if (num1==0) {
+    console.log("El número",num1,"es neutro")
+}
+else{
+    console.log("El número",num1,"es negativo")
+}
+*/
+
+alert("ejercicio 2")
+edad=prompt("Ingresa tu edad")
+
+if (edad<18) {
+    console.log("Eres menor de edad")
+}
+else if (edad>18) {
+    console.log("Eres mayor de edad")
+}
